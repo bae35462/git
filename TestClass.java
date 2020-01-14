bcf943c004702662510a28211ed6860e1e1c6c20
bae35462 <bae35462@naver.com>
2020-01-14 15:49:13 +0900

    add gitignore

--- TestClass.java
+++ TestClass.java
@@ -3,11 +3,7 @@ package test;
 public class TestClass {
 
     public static void main (String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello~!");
-=======
         System.out.println("Hi~!!!!!!!");
->>>>>>> new_branch
         System.out.println("Config");
     }
 }
