e5a5e638e1e1b0b2130d66a7e4403043a8e6f930
bae35462 <bae35462@naver.com>
2020-01-14 16:09:53 +0900

    local change

--- TestClass.java
+++ TestClass.java
@@ -4,6 +4,6 @@ public class TestClass {
 
     public static void main (String[] args) {
         System.out.println("Hi~!!!!!!!");
-        System.out.println("Config");
+        System.out.println("gipgip");
     }
 }
